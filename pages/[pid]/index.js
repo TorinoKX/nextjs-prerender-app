b218a3ed772b0a853491f79c4d911b3cbacedf08
Zak Cobham-Davis <zakcdavis@gmail.com>
2022-09-22 19:50:36 +1000

    prerendering dynamic path with data from json instead of hardcoding

--- pages/[pid]/index.js
+++ pages/[pid]/index.js
@@ -6,7 +6,6 @@ import { Fragment } from "react";
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
 
-
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -15,14 +14,20 @@ function ProductDetailPage(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -34,9 +39,15 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
-    fallback: "blocking",
+    paths: pathsWithParams,
+    fallback: false,
   };
 }
 
